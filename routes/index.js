d91cc624e6db1467d8fcdf043a61850ef6654caf
Adithyan_A <deepaadithyan56@gmail.com>
2021-04-04 22:56:57 +0530

    New routes to goto full_url, post new url and delte_url was added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const short_urls = require('../models/short_url');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', async (req, res, next) => {
+    const aurls_urls = await short_urls.find();
+    res.render('index', { aurls_urls: aurls_urls });
+});
+
+router.post('/aurlshort', async (req, res) => {
+    await short_urls.create({ full_url: req.body.url_field });
+    res.redirect('/');
+});
+
+router.get('/delete/:aurls', async (req, res, next) => {
+    const item = await short_urls.deleteOne({ short_url: req.params.aurls });
+    if(item == null) return res.sendStatus(404)
+    res.redirect('/');
+});
+
+router.get('/:aurls', async (req, res, next) => {
+    // This is now used like else case
+    const item = await short_urls.findOne({ short_url: req.params.aurls });
+    if(item == null) return res.sendStatus(404)
+    item.clicks++
+    item.save()
+    res.redirect(item.full_url)
 });
 
 module.exports = router;
